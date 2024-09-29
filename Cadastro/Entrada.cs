9a4ec8a82d96026f7abea89776704854044e19d0
Julia <juliapinheiro620@gmail.com>
2024-09-28 21:45:02 -0300

    Add project files.

---
+++ Cadastro/Entrada.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    internal class Entrada
+    {
+        public string Email {  get; private set; }
+        public string Senha { get; private set; }
+        public string Nome { get; private set; }
+
+        public void NomePessoa(string nomePessoa)
+        {
+            Nome = nomePessoa;
+        }
+
+        public void Cadastro(string emailCadastro,string senhaCadrastro)
+        {
+            Email = emailCadastro;
+            Senha = senhaCadrastro;
+        }
+
+        public void Entrando(string emailEntrada,string senhaEntrada)
+        {
+            while(emailEntrada != Email || senhaEntrada != Senha)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Informações erradas! Tente novamente.");
+                Console.Write("Digite seu email: ");
+                emailEntrada = Console.ReadLine();
+                Console.Write("Digite sua senha: ");
+                senhaEntrada = Console.ReadLine();
+                
+            }
+            Console.WriteLine($"Seja bem-vindo, {Nome}");
+            
+
+
+        }
+    }
+}
---
+++ Cadastro/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Entrada entrada = new Entrada();
+            Console.Write("Qual o seu nome? ");
+            string nome = Console.ReadLine();
+            entrada.NomePessoa(nome);
+            
+
+            Console.Write("Cadrastre o seu email: ");
+            string emailCadrastro = Console.ReadLine();
+            Console.Write("Cadrastre sua senha: ");
+            string senhaCadrastro = Console.ReadLine();
+            entrada.Cadastro(emailCadrastro, senhaCadrastro);
+
+            Console.WriteLine("Suas informações foram cadastradas!");
+            Console.Write("Deseja entrar no programa (s/n): ");
+            char escolha = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            while (escolha != 's' && escolha != 'n')
+            {
+                Console.WriteLine("Opção Inválida! Tente novamente.");
+                Console.Write("Deseja entrar no programa (s/n): ");
+                escolha = char.Parse(Console.ReadLine());
+            }
+
+            if(escolha == 's')
+            {
+                Console.Write("Digite seu email: ");
+                string emailEntrada = Console.ReadLine();
+                Console.Write("Digite sua senha: ");
+                string senhaEntrada = Console.ReadLine();
+                entrada.Entrando(emailEntrada, senhaEntrada);
+            }
+            else
+            {
+                Console.WriteLine("Obrigado por se cadastrar.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
